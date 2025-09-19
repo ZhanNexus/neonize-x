eafd008b4257ee96979196dc0a28a5690377f07d
ZulX88 <zhanprasetyaningrat@gmail.com>
2025-09-20 00:01:03 +0700

    little

--- tools/goneonize.py
+++ tools/goneonize.py
@@ -9,11 +9,8 @@ from typing import Dict
 import glob
 
 cwd = (Path(__file__).parent.parent / "goneonize/").__str__()
-# shell = [
-#     "protoc --go_out=. Neonize.proto def.proto",
-#     "protoc --python_out=../neonize/proto --mypy_out=../neonize/proto def.proto Neonize.proto",
-#     "protoc --go_out=. --go-grpc_out=. -I . Neonize.proto def.proto",
-# ]
+
+# Perintah protoc
 shell = [
     "protoc --go_out=. --go_opt=paths=source_relative Neonize.proto",
     "protoc --python_out=../../neonize/proto --mypy_out=../../neonize/proto Neonize.proto",
@@ -21,7 +18,6 @@ shell = [
         f"protoc --python_out=../../neonize/proto --mypy_out=../../neonize/proto {path}"
         for path in glob.glob("*/*.proto", root_dir=cwd + "/defproto")
     ],
-    # "protoc --go_out=. --go-grpc_out=. -I . Neonize.proto def.proto",
 ]
 
 
@@ -47,74 +43,91 @@ def generated_name(os_name="", arch_name=""):
     return f"neonize-{os_name}-{arch_name}.{ext}"
 
 
-def __build():
-    args = argparse.ArgumentParser()
-    args.add_argument("--os", default=platform.system().lower())
-    args.add_argument("--arch", default=platform.machine().lower())
-    parse = args.parse_args()
-    filename = generated_name(parse.os, parse.arch)
-    for sh in shell:
-        subprocess.call(shlex.split(sh), cwd=cwd)
-    if (Path(cwd) / "defproto").exists():
-        shutil.rmtree(f"{cwd}/defproto")
-    os.mkdir(f"{cwd}/defproto")
-    os.rename(
-        f"{cwd}/github.com/krypton-byte/neonize/defproto/",
-        f"{cwd}/defproto")
-    shutil.rmtree(f"{cwd}/github.com")
-    subprocess.call(
-        shlex.split(
-            f"go build -buildmode=c-shared -ldflags=-s -o {filename} main.go"),
-        cwd=cwd,
-        env=os.environ.update({"CGO_ENABLED": "1"}),
-    )
-    if (Path(cwd).parent / filename).exists():
-        os.remove(os.path.dirname(cwd) + "/" + filename)
-    os.rename(f"{cwd}/{filename}", os.path.dirname(cwd) + "/" + filename)
-
-
 def build_proto():
+    # copy Neonize.proto ke defproto
     with open(cwd + "/Neonize.proto", "rb") as file:
         with open(cwd + "/defproto/Neonize.proto", "wb") as wf:
             wf.write(file.read())
+
+    # jalankan semua perintah protoc
     for sh in shell:
         subprocess.call(shlex.split(sh), cwd=cwd + "/defproto")
-    # if (Path(cwd) / "defproto").exists():
-    #     shutil.rmtree(f"{cwd}/defproto")
-    # os.mkdir(f"{cwd}/defproto")
-    # os.rename(f"{cwd}/github.com/krypton-byte/neonize/defproto/", f"{cwd}/defproto")
-    # shutil.rmtree(f"{cwd}/github.com")
 
 
 def build_neonize():
     os_name = os.environ.get("GOOS") or platform.system().lower()
     arch_name = os.environ.get("GOARCH") or platform.machine().lower()
     print(f"os: {os_name}, arch: {arch_name}")
+
     filename = generated_name(os_name, arch_name)
-    print(filename)
+    print("output:", filename)
+
+    env = os.environ.copy()
+    env.update({"CGO_ENABLED": "1"})
+
+    # compile Go jadi .so
     subprocess.call(
         shlex.split(
-            f"go build -buildmode=c-shared -ldflags=-s -o {filename} "),
+            f"go build -buildmode=c-shared -ldflags=-s -o {filename} main.go"
+        ),
         cwd=cwd,
-        env=os.environ.update({"CGO_ENABLED": "1"}),
+        env=env,
     )
-    if (Path(cwd).parent / f"neonize/{filename}").exists():
-        os.remove(os.path.dirname(cwd) + "/neonize/" + filename)
+
+    target = Path(cwd).parent / "neonize" / filename
+    if target.exists():
+        target.unlink()
+
+    os.rename(f"{cwd}/{filename}", target)
+
+
+def build_android():
+    filename = generated_name("android", "arm64")
+
+    # prepare environment Android NDK
+    env = os.environ.copy()
+    env.update({
+        "CGO_ENABLED": "1",
+        "CC": "/home/krypton-byte/Pictures/android-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android28-clang",
+        "CXX": "/home/krypton-byte/Pictures/android-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android28-clang++",
+    })
+
+    for sh in shell:
+        subprocess.call(shlex.split(sh), cwd=cwd)
+
+    if (Path(cwd) / "defproto").exists():
+        shutil.rmtree(f"{cwd}/defproto")
+    os.mkdir(f"{cwd}/defproto")
     os.rename(
-        f"{cwd}/{filename}",
-        os.path.dirname(cwd) +
-        "/neonize/" +
-        filename)
+        f"{cwd}/github.com/krypton-byte/neonize/defproto/",
+        f"{cwd}/defproto"
+    )
+    shutil.rmtree(f"{cwd}/github.com")
+
+    subprocess.call(
+        shlex.split(
+            f"go build -buildmode=c-shared -ldflags=-s -o {filename} main.go"
+        ),
+        cwd=cwd,
+        env=env,
+    )
+
+    target = Path(cwd).parent / filename
+    if target.exists():
+        target.unlink()
+
+    os.rename(f"{cwd}/{filename}", target)
 
 
 def build():
     args = argparse.ArgumentParser()
     sub = args.add_subparsers(dest="build", required=True)
     sub.add_parser("goneonize")
-    # arg.add_argument("--out", type=str, default=os.path.dirname(cwd) + "/neonize/")
     sub.add_parser("proto")
     sub.add_parser("all")
+    sub.add_parser("android")
     parse = args.parse_args()
+
     match parse.build:
         case "goneonize":
             build_neonize()
@@ -123,42 +136,8 @@ def build():
         case "all":
             build_proto()
             build_neonize()
-
-
-def build_android():
-    filename = generated_name("android", "aarch4")
-    for sh in shell:
-        subprocess.call(shlex.split(sh), cwd=cwd)
-    if (Path(cwd) / "defproto").exists():
-        shutil.rmtree(f"{cwd}/defproto")
-    os.mkdir(f"{cwd}/defproto")
-    os.rename(
-        f"{cwd}/github.com/krypton-byte/neonize/defproto/",
-        f"{cwd}/defproto")
-    shutil.rmtree(f"{cwd}/github.com")
-    os.environ.update(
-        {
-            "CGO_ENABLED": "1",
-            "CC": "/home/krypton-byte/Pictures/android-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android28-clang",
-            "CXX": "/home/krypton-byte/Pictures/android-ndk-r26b/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android28-clang++",
-        }
-    )
-    subprocess.call(
-        shlex.split(
-            f"go build -buildmode=c-shared -ldflags=-s -o {filename} main.go"),
-        cwd=cwd,
-        env=os.environ,
-    )
-    if (Path(cwd).parent / filename).exists():
-        os.remove(os.path.dirname(cwd) + "/" + filename)
-    os.rename(f"{cwd}/{filename}", os.path.dirname(cwd) + "/" + filename)
-    # command = shlex.split("build.bat " if os.name == "nt" else "bash build.sh "+platform.machine())
-    # subprocess.call(
-    #     command,
-    #     cwd=os.path.dirname(__file__),
-    #     env=os.environ.update({"build_neonize": "1"}),
-    #     shell=os.name == "nt",
-    # )
+        case "android":
+            build_android()
 
 
 if __name__ == "__main__":
